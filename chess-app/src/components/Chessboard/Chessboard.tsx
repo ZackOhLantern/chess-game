3f1cb3fee44bc4acfce803b4fbf4c012e8d8a1c4
ZackOhLantern <z.cuervo@live.com>
2022-09-25 12:05:55 -0700

    piece generation

--- chess-app/src/components/Chessboard/Chessboard.tsx
+++ chess-app/src/components/Chessboard/Chessboard.tsx
@@ -13,6 +13,24 @@ interface Piece {
 //This is a pieces object with a Piece Interface!
 const pieces: Piece[] = [];
 
+for(let p=0; p<2; p++) {
+    const type = (p === 0) ? "black" : "white"
+    const y = (p === 0) ? 7 : 0;
+
+    pieces.push({ image: `assets/images/rook-${type}.png`, x:0, y:y})
+    pieces.push({ image: `assets/images/rook-${type}.png`, x:7, y:y})
+
+    pieces.push({ image: `assets/images/knight-${type}.png`, x:1, y:y})
+    pieces.push({ image: `assets/images/knight-${type}.png`, x:6, y:y})
+
+    pieces.push({ image: `assets/images/bishop-${type}.png`, x:2, y:y})
+    pieces.push({ image: `assets/images/bishop-${type}.png`, x:5, y:y})
+
+    pieces.push({ image: `assets/images/king-${type}.png`, x:4, y:y})
+    pieces.push({ image: `assets/images/queen-${type}.png`, x:3, y:y})
+
+}
+
 //duplicates pawn image 8 times across the board
 for(let i = 0; i <8; i++) {
     pieces.push({image: "assets/images/pawn-black.png", x: i, y: 6});
@@ -22,8 +40,10 @@ for(let i = 0; i <8; i++) {
     pieces.push({image: "assets/images/pawn-white.png", x: i, y: 1});
 }
 
+
+
 //assigns an image to every Piece.image. AND assigns image to every coordinate in board.
-export default function Chessboard() {
+ export default function Chessboard() {
     let board = [];
 
     for(let j=verticalAxis.length-1; j >= 0; j--) {
@@ -42,6 +62,5 @@ export default function Chessboard() {
     }
     return <div id="chessboard">{board}     
     </div>
-}
-
+} 
 //timestamp 16:51
